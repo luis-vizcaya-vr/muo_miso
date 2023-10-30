4f660a5f53dbaebdf8b045ad668a0c4c63afb045
Luis Vizcaya <luis.vizcaya@woodmac.com>
2023-10-30 10:34:39 -0500

    make some modifications in substation.py

--- src/substation.py
+++ src/substation.py
@@ -53,46 +53,7 @@ class Substation:
     
         
     def train_model(self, drop_cols = None, actual_col = None):
-        
-        model_dict = {}
-        if self.df_train.shape[0] < 700:
-            print(f"{self.name} does not have enough data.")
-        else:
-            print(f"{self.name} has {self.df_train.shape[0]} training observations.")
-            
-        zonal_demand = self.df_train[["ts_hour_floor", "actuals_substation_total"]].merge(Simulator.zonal_demand_hist, how = "left", on = "ts_hour_floor")
-        zonal_lmps = self.df_train[["ts_hour_floor", "actuals_substation_total"]].merge(Simulator.zonal_lmps_hist, how = "left", on = "ts_hour_floor")
-        best_zonal_demand = self.find_max_corr(zonal_demand, "actuals_substation_total")
-        zonal_demand = zonal_demand[["ts_hour_floor", best_zonal_demand]]
-        best_zonal_lmp = self.find_max_corr(zonal_lmps, "actuals_substation_total")
-        zonal_lmps = zonal_lmps[["ts_hour_floor", best_zonal_lmp]]
-        X_train = self.df_train.merge(zonal_demand, how = "left", on = "ts_hour_floor")
-        X_train = X_train.merge(zonal_lmps, how = "left", on = "ts_hour_floor")
-        scaler = StandardScaler()
-        X_train = X_train.drop(columns = drop_cols)
-        X_train = X_train.fillna(X_train.median())
-        model_dict["medians"] = X_train.median()
-        cols = X_train.columns.to_list()
-        model_dict["ordered_columns"] = cols
-        X_train = scaler.fit_transform(X_train)
-        model_dict["scaler"] = scaler
-        y_train = self.df_train[actual_col]
-        
-        if self.zero_band is not None:
-            y_train = np.where(y_train < self.zero_band, 0, y_train)
-        
-        model_dict["maximum"] = y_train.max()
-        parameters = {"n_estimators" : [100], "min_samples_leaf" : [1], "min_samples_split" : [2], "max_depth" : [None]}
-        clf = GridSearchCV(RandomForestRegressor(), parameters, scoring = "neg_mean_squared_error")
-        clf.fit(X_train, y_train)
-        best_model = clf.best_estimator_
-        model_dict["model"] = best_model
-        
-        with open(f"C:/Users/i32213/OneDrive - Wood Mackenzie Limited/Desktop/MUO/muov2-training/debug/individual models/model_{self.name}_{model_run_date}.pkl", "wb") as f:
-                    pickle.dump(model_dict, f)
-
-        return model_dict
-    
+         pass
 
     def merge_renew_metadata(self):
         renew_meta = self.renew_forecast.merge(self.metadata, how = "left", on = "unit_id")
@@ -137,52 +98,18 @@ class Substation:
         return merged_df
     
     def merge_renew(merged_df, renew_meta):
-        merged_df = merged_df.merge(renew_meta, how = "outer", on = ["ts_hour_floor", "Substation"])
-        return merged_df
-    
+        pass
+
     def get_train_test(self, split = True):
-        
-        # MERGE RENEWABLE FORECASTS AND METADATA
-        renew_meta = self._merge_renew_metadata()
-        
-        dates_voltages = self._merge_dates_voltages(self._get_complete_date_range())
-        
-        # MERGE ACTUALS AND METADATA
-        actuals_meta = self._merge_actuals_metadata()
-        
-        # MERGE VOLTAGES AND ACTUALS
-        merged_df = self._merge_voltages_actuals(dates_voltages, actuals_meta)
-        
-        # MERGE LMPS
-        merged_df = self._merge_lmps(merged_df)
-        
-        # MERGE RENEWABLE FORECASTS
-        merged_df = self._merge_renew(merged_df, renew_meta)
-        
-        merged_df = merged_df[["ts_hour_floor", "Substation", "GeneratorName", "ResourceNode", "voltage", "LMP", "renew_forecast_substation_total", "actuals_substation_total"]]
-        
-        if split == True:
-            df_train = merged_df[(merged_df.ts_hour_floor >= self.start_date) & (merged_df.ts_hour_floor < self.cutoff_date) & (merged_df.GeneratorName.notnull())]
-            
-            df_test = merged_df[(merged_df.ts_hour_floor >= self.cutoff_date) & (merged_df.ts_hour_floor <= self.end_date) & (merged_df.GeneratorName.notnull())]
-            
-            return df_train, df_test
-        else:
-            return merged_df
-    
+        pass
     
     def set_sub_dates(self, start_date, cutoff_date, end_date):
-        
         self.start_date = start_date
         self.cutoff_date = cutoff_date
         self.end_date = end_date
              
     def set_sub_df_train_test(self, df):
-        
-        df_train, df_test = df
-        self.df_train = df_train[["ts_hour_floor", "Substation", "voltage", "LMP", "renew_forecast_substation_total", "actuals_substation_total"]]
-        self.df_test = df_test[["ts_hour_floor", "Substation", "voltage", "LMP", "renew_forecast_substation_total", "actuals_substation_total"]]
-    
+        pass
         
     def set_sub_exclude_period(self, exclude_period):    
         self.exclude_period = exclude_period
